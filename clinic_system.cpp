75a70f14b6a0f04aec98bb51b04087e4a7bd0a2b
Nour Mohamed <161865218+Nour5080@users.noreply.github.com>
2025-07-23 03:59:08 +0300

    Update clinic_system.cpp

--- clinic_system.cpp
+++ clinic_system.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 const int max_spec=20;
@@ -8,25 +9,75 @@ string names[max_spec+1][max_queue];
 int status[max_spec+1][max_queue];
 int queue_length[max_spec+1];
 
-int menu () {
-    int choise;
-    choise = -1;
-    while (choise == -1) {
-        cout << "Enter Your Choise\n";
+bool is_text(string input) {
+    if (input.empty()) return false;
+
+    for (int i = 0; i < input.length(); i++) {
+        char c = input[i];
+
+        if (!((c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            c == ' ')) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+bool is_number(string input) {
+    if (input.empty()) return false;
+
+    for (int i = 0; i < input.length(); i++) {
+        char c = input[i];
+
+        if (c < '0' || c > '9') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+bool valid(int choice) {
+    return (choice >= 1 && choice <= 4);
+}
+
+int menu() {
+    int choice;
+
+    while (true) {
+        cout << "Enter Your Choice\n";
         cout << "1) Add new patient\n";
         cout << "2) Print all patients\n";
         cout << "3) Get next patient\n";
         cout << "4) Exit\n";
-        cin >> choise;
+        cout << ">> ";
 
-    if(!(choise>=1&&choise<5)){
-        cout<<"invalid choice. TRY again\n";
-        choise=-1;                                                                                      //keep loop
+        cin >> choice;
+
+        
+        if (cin.fail()) {
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            cout << "Invalid input. Please enter a number between 1 and 4.\n\n";
+            continue;
+        }
+
+        if (!valid(choice)) {
+            cout << "Invalid choice. Please try again.\n\n";
+            continue;
+        }
+
+        break; 
     }
-}
-return choise;
 
+    return choice;
 }
+
 void shift_rhight(int spec,string names_sp[],int status_sp[]){
     int len =queue_length[spec];
     for(int i = len-1;i >= 0;--i){
@@ -47,8 +98,52 @@ bool add_patient(){
     int spec;
     int st;
     string name;
-    cout<<"Enter spcialization, name, statis(0 for regular // 1 for urgent):\n";
-    cin>>spec>>name>>st;
+    
+    while (true) {
+
+        string spcialization;
+        cout << "Enter spcialization:\n\n";
+        cin >> spcialization;
+        if (is_number(spcialization)) {
+            spec = stoi(spcialization);
+            break;
+        }
+        else {
+            cout << "invaild spcialization, please try again.\n\n";
+        }
+    }
+    while (true) {
+
+        string Name;
+        cout << "Enter Name:\n\n";
+        cin >> Name;
+        if (is_text(Name)) {
+            name = Name;
+            break;
+        }
+        else {
+            cout << "invaild Name, please try again.\n\n";
+        }
+    }
+    while (true) {
+
+        string statis;
+        cout << "statis(0 for regular // 1 for urgent):\n";
+        cin >> statis;
+        if (is_number(statis)) {
+            st = stoi(statis);
+            if (st == 0 || st == 1) {
+                break; // true
+            }
+            else {
+                cout << "only allowed 0 or 1.\n";
+            }
+        }
+        else {
+            cout << "invaild statis, please try again.\n";
+        }
+    }
+     
     int position=queue_length[spec];
     if(position>=max_queue){
         cout<<"sorry we can't add more patient for this specialization \n";
@@ -95,8 +190,19 @@ void print_patients(){
 }
 void get_next_patient(){
 int spec;
-cout<<"enter specialization\n";
-cin>>spec;
+while (true) {
+
+    string spcialization;
+    cout << "Enter spcialization";
+    cin >> spcialization;
+    if (is_number(spcialization)) {
+        spec = stoi(spcialization);
+        break;
+    }
+    else {
+        cout << "invaild spcialization, please try again.";
+    }
+}
 int len =queue_length[spec];
 if (len==0){
     return;
